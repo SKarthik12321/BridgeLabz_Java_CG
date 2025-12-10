3ffe61ff68c6b5180858b87e1116df64492eb1ed
S Karthik <skarthik.link@gmail.com>
2025-12-10 11:26:47 +0530

    Initial commit: Add program

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -4,6 +4,9 @@ public class LeapYear {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int newYear = sc.nextInt();
+        if (newYear%400==0){
+            System.out.p
+        }
 
     }
 }
